689037e9867dc961ada41e7faff3a4d8a67902ee
Samuel Meyo <masyome@gmail.com>
2023-05-15 09:44:18 -0700

    fix

---
+++ models/review.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This module contains a class that inherits
+from BaseModel
+"""
+
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """
+    This class inherits from BaseModel
+
+    =====  Public Class Attributes =====
+    place_id => string empty string
+    user_id => string - empty string
+    text => string - empty string
+    """
+
+    place_id = ""
+    user_id = ""
+    text = ""
